82d7f71f81e76a00df9b58d9a92d0e3744624347
ArcStatic <eband42@gmail.com>
2018-01-27 04:24:08 +0000

    Found a better pe parsing library

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,22 @@ use std::string::String;
 extern crate subprocess;
 use subprocess::{Popen, PopenConfig, Exec};
 
-extern crate libc;
-extern crate bindgen;
-use bindgen::builder;
-
-extern crate pe;
-use pe::Pe;
-use pe::types::{SectionHeader, PeHeader};
+extern crate winapi;
+use std::ffi::OsStr;
+use std::iter::once;
+use std::os::windows::ffi::OsStrExt;
+use std::ptr::null_mut;
+use winapi::um::winuser::{MB_OK, MessageBoxW};
 
 extern crate bytes;
 use bytes::Bytes;
 
 extern crate byteorder;
 use byteorder::{ByteOrder, BigEndian, LittleEndian};
+
+extern crate pelite;
+use pelite::pe64::{Pe, PeFile};
+use pelite::image::IMAGE_DOS_HEADER;
 
 struct ProcessData {
 	pidh : u8,
@@ -57,17 +60,32 @@ fn main() {
 	let mut legit_proc = Exec::shell("C:\\windows\\system32\\lsass.exe").detached();
 	//legit_proc.popen();
 	
+	/*
 	let mal_parse = Pe::new(&mal_buf).unwrap();
 	let img_file_header = mal_parse.get_header();
 	
+	//Test to see if PE parsing has worked
+	//s_buf should output "PE[null][null]" when converted to str
 	let mut s_buf = [0; 4];
 	let signature = img_file_header.signature;
 	LittleEndian::write_u32(&mut s_buf, signature);
 	let opt_header = &img_file_header.size_of_optional_header;
 	println!("PE parse:\n{:?} -> {:?}\n{:?}\n", signature, from_utf8(&s_buf).unwrap(), opt_header);
 	
-	let mal_data = ProcessData{pidh : mal_buf[0], pinh : mal_buf[1]};
 	//thread::sleep(time::Duration::from_millis(1000));
+	*/
+	
+	let mal_data = ProcessData{pidh : mal_buf[0], pinh : mal_buf[1]};
+	
+	let mal_pe = PeFile::from_bytes(&mal_buf).unwrap();
+	let dos_head = mal_pe.dos_header();
+	let nt_head = mal_pe.nt_headers();
+	let section_head = mal_pe.section_headers();
+	
+	println!("PE e_magic:{:?}\n", mal_pe.dos_header().e_magic);
+	
+	println!("legit_proc:\n{:?}", legit_proc);
+	
 	println!("mal process created in suspended state.\n");
 	println!("Process data:\npidh: {:?}\npinh: {:?}", mal_data.pidh, mal_data.pinh)
 	
